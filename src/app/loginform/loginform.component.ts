470653745a301e8faa20f237f90ae03e2475da9c
IrynaMe <irinam7a@gmail.com>
2024-06-07 13:53:03 +0200

    mostraCategoria in login form

--- src/app/loginform/loginform.component.ts
+++ src/app/loginform/loginform.component.ts
@@ -8,20 +8,20 @@ import { GetCategorieService } from '../get-categorie.service';
   standalone: true,
   imports: [],
   templateUrl: './loginform.component.html',
-  styleUrl: './loginform.component.css'
+  styleUrl: './loginform.component.css',
 })
 export class LoginformComponent {
-  route: ActivatedRoute=inject(ActivatedRoute);
-  idInserito: number=-1;
+  route: ActivatedRoute = inject(ActivatedRoute);
+  idInserito: number = -1;
   miaCategoria: CategoriaUtente | undefined;
+  catToShow: string = '';
   GetCategorieServiceInst: GetCategorieService = inject(GetCategorieService);
 
-  constructor(private router: Router){
-    this.idInserito=this.route.snapshot.params['appId'];
-    this.miaCategoria=this.GetCategorieServiceInst.getCategoriaById(this.idInserito);
-
-    }
+  constructor(private router: Router) {
+    this.idInserito = this.route.snapshot.params['appId'];
+    this.miaCategoria = this.GetCategorieServiceInst.getCategoriaById(
+      this.idInserito
+    );
+    this.catToShow = this.miaCategoria?.nomeCat||"";
   }
-
-
-
+}
